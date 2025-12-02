e15c48db35dd664c9ee15b3bb08e1b2652894685
Shenal Parmar <169513116+shenal-parmar@users.noreply.github.com>
2025-12-02 11:27:29 +0530

    issue of navbar resolved

--- frontend/pages/Navbar.jsx
+++ frontend/pages/Navbar.jsx
@@ -29,16 +29,16 @@ export default function Navbar() {
   return (
     <nav className="bg-white/80 backdrop-blur-md shadow-md px-4 sm:px-6 py-4 flex justify-between items-center border-b border-teal-100 relative">
       {/* Brand */}
-      <Link
+      <h1
         className="py-6 text-xl sm:text-2xl font-bold text-teal-700 cursor-pointer hover:text-teal-800 transition"
-         to={user.role === "admin" ? "/admin-dashboard" : "/employee-dashboard"}
+        onClick={() => navigate("/")}
       >
      <img
   src="/image2.avif"
   alt="emslogo"
   className="absolute top-2 h-16 w-auto object-contain" 
 /> 
-      </Link>
+      </h1>
 
       {/* Desktop Menu */}
       <div className="hidden md:flex items-center gap-6">
